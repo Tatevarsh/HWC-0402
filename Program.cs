77e8206816f7f7f165b94568fc27c5dbb1be6cd6
Tatevik <tshavarshevna@gmail.com>
2023-02-05 22:43:21 +0300

    factorial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 1;
+
+for (int i = 2; i <= num; i++)
+{
+   
+    sum = sum * i;
+
+}
+   Console.WriteLine(sum);
+
+
+
+
+// }
+// }
+// if (count > 1)
+// {
+   
+//     a = count*count;
+   
+//     Console.WriteLine($"= {a} ");
+// } 
+    
